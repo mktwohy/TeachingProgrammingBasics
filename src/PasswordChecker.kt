00b3be5e8722d9a31bc2c56eb2078543f5f56efe
mktwohy <mktwohy@gmail.com>
2021-12-07 23:38:40 -0600

    commit

--- src/PasswordChecker.kt
+++ src/PasswordChecker.kt
@@ -1,3 +1,20 @@
 fun main() {
-    // create
+    // create password variable (String)
+    var password: String
+    // create variable for storing user input (String)
+    var userInput: String
+
+
+    // fill password variable with a password
+    password = "password"
+    // get String input from user (have them enter password)
+    userInput = "password"
+
+    // if the password and input are the same, print "logging in". otherwise, print "incorrect password"
+    if (userInput == password){
+        print("logging in")
+    }
+    else {
+        print("incorrect password")
+    }
 }
